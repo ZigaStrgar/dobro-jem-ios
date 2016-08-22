64adca964e106b816e40bfec532d32d5baf62fcb
Ziga Strgar <ziga_strgar@hotmail.com>
2016-08-22 12:12:37 +0200

    Initial Commit

---
+++ Dobro jem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dobro jem
+//
+//  Created by Ziga Strgar on 22/08/16.
+//  Copyright © 2016 Ziga Strgar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
